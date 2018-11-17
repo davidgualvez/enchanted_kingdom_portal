8e21be77bb76692dba6d099d7ccbabbdd40d55c6
david gualvez <gualvezdavid@gmail.com>
2018-11-17 09:35:57 +0800

    ..

---
+++ resources/views/pages/customers/components/products.blade.php
@@ -0,0 +1,175 @@
+<h4 class="ui horizontal divider header">
+  <i class="gift icon"></i>
+  Our Products
+</h4>
+
+<div class="ui link six doubling cards">
+  <div class="card">
+	    <div class="image">
+	      <img src="https://source.unsplash.com/random/250x250">
+	    </div>
+	    <div class="content">
+	      <div class="header">Matt Giampietro</div>
+	      <div class="meta">
+	        <a>Friends</a>
+	      </div>
+	      <div class="description">
+	        Matthew is an interior designer living in New York.
+	      </div>
+	    </div>
+	    <div class="extra content">
+	      <span class="right floated">
+	        <!-- Joined in 2013 -->
+	        <div class="ui vertical animated button" tabindex="0">
+	          <div class="hidden content">Shop</div>
+	          <div class="visible content">
+	            <i class="shop icon"></i>
+	          </div>
+	        </div>
+	      </span>
+	      <span>
+	        <!-- <i class="user icon"></i>
+	        75 Friends -->
+	        <a class="ui tag label">P 0.00</a>
+	      </span>
+	    </div>
+  </div> 
+
+   <div class="card">
+	    <div class="image">
+	      <img src="https://source.unsplash.com/random/250x250">
+	    </div>
+	    <div class="content">
+	      <div class="header">Matt Giampietro</div>
+	      <div class="meta">
+	        <a>Friends</a>
+	      </div>
+	      <div class="description">
+	        Matthew is an interior designer living in New York.
+	      </div>
+	    </div>
+	    <div class="extra content">
+	      <span class="right floated">
+	        <!-- Joined in 2013 -->
+	        <div class="ui vertical animated button" tabindex="0">
+	          <div class="hidden content">Shop</div>
+	          <div class="visible content">
+	            <i class="shop icon"></i>
+	          </div>
+	        </div>
+	      </span>
+	      <span>
+	        <!-- <i class="user icon"></i>
+	        75 Friends -->
+	        <a class="ui tag label">P 0.00</a>
+	      </span>
+	    </div>
+  </div>
+
+   <div class="card">
+	    <div class="image">
+	      <img src="https://source.unsplash.com/random/250x250">
+	    </div>
+	    <div class="content">
+	      <div class="header">Matt Giampietro</div>
+	      <div class="meta">
+	        <a>Friends</a>
+	      </div>
+	      <div class="description">
+	        Matthew is an interior designer living in New York.
+	      </div>
+	    </div>
+	    <div class="extra content">
+	      <span class="right floated">
+	        <!-- Joined in 2013 -->
+	        <div class="ui vertical animated button" tabindex="0">
+	          <div class="hidden content">Shop</div>
+	          <div class="visible content">
+	            <i class="shop icon"></i>
+	          </div>
+	        </div>
+	      </span>
+	      <span>
+	        <!-- <i class="user icon"></i>
+	        75 Friends -->
+	        <a class="ui tag label">P 0.00</a>
+	      </span>
+	    </div>
+  </div>
+
+   <div class="card">
+	    <div class="image">
+	      <img src="https://source.unsplash.com/random/250x250">
+	    </div>
+	    <div class="content">
+	      <div class="header">Matt Giampietro</div>
+	      <div class="meta">
+	        <a>Friends</a>
+	      </div>
+	      <div class="description">
+	        Matthew is an interior designer living in New York.
+	      </div>
+	    </div>
+	    <div class="extra content">
+	      <span class="right floated">
+	        <!-- Joined in 2013 -->
+	        <div class="ui vertical animated button" tabindex="0">
+	          <div class="hidden content">Shop</div>
+	          <div class="visible content">
+	            <i class="shop icon"></i>
+	          </div>
+	        </div>
+	      </span>
+	      <span>
+	        <!-- <i class="user icon"></i>
+	        75 Friends -->
+	        <a class="ui tag label">P 0.00</a>
+	      </span>
+	    </div>
+  </div>
+
+   <div class="card">
+	    <div class="image">
+	      <img src="https://source.unsplash.com/random/250x250">
+	    </div>
+	    <div class="content">
+	      <div class="header">Matt Giampietro</div>
+	      <div class="meta">
+	        <a>Friends</a>
+	      </div>
+	      <div class="description">
+	        Matthew is an interior designer living in New York.
+	      </div>
+	    </div>
+	    <div class="extra content">
+	      <span class="right floated">
+	        <!-- Joined in 2013 -->
+	        <div class="ui vertical animated button" tabindex="0">
+	          <div class="hidden content">Shop</div>
+	          <div class="visible content">
+	            <i class="shop icon"></i>
+	          </div>
+	        </div>
+	      </span>
+	      <span>
+	        <!-- <i class="user icon"></i>
+	        75 Friends -->
+	        <a class="ui tag label">P 0.00</a>
+	      </span>
+	    </div>
+  </div>
+</div> 
+<div class="ui grid">
+	<div class="column">
+		<div class="tiny ui buttons">
+		  <button class="ui button">
+		  	<i class="angle left icon"></i>
+		  	Prev
+		  </button> 
+		  <button class="ui button">
+		  	<i class="angle right icon"></i>
+		  	Next
+		  </button>
+		</div>
+	</div> 
+</div> 
---
+++ resources/views/pages/customers/components/slider.blade.php
@@ -0,0 +1,12 @@
+<div class="slider slider1" style="height: 500px;">
+  <div class="slides">
+    <div class="slide-item item1"> 
+    </div>
+    <div class="slide-item item2"> 
+    </div>
+    <div class="slide-item item3">
+    </div>
+    <div class="slide-item item4">
+    </div>
+  </div>
+</div>
