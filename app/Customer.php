e8a8bda35c75433765202e0f23bb6f7e11d35ff3
david <gualvezdavid@gmail.com>
2019-01-03 21:54:06 +0800

    ..

--- app/Customer.php
+++ app/Customer.php
@@ -4,10 +4,34 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Sofa\Eloquence\Eloquence; // base trait
+use Sofa\Eloquence\Mappable; // extension trait
+use Sofa\Eloquence\Mutable; // extension trait
+
 class Customer extends Model
 {
+    use Eloquence, Mappable, Mutable;
     //
     protected $table = 'Customers'; 
+
+    //model mapping
+    protected $maps = [
+        'branch_id' => 'BRANCHID',
+        'customer_id' => 'CUSTOMERID',
+        'customer_code' => 'CUSTOMERCODE',
+        'customer_type' => 'CUSTOMERTYPE',
+        'name' => 'NAME',
+        'address' => 'ADDRESS',
+        'tin' => 'TIN',
+        'business_style' => 'BUSINESSSTYLE',
+        'user_id' => 'USER_ID',
+        'wallet' => 'WALLET', 
+        'points' => 'POINTS',
+        'mobile_number' => 'MOBILE_NUMBER',
+        'birthdate' => 'BIRTHDATE',
+        'is_loyalty' => 'IS_LOYALTY',
+        'is_inhouse' => 'IS_INHOUSE'
+    ];
 
     /**
      * Relationship
