e5980e714d6b9444dc89f079e17ab45d76582b59
david <gualvezdavid@gmail.com>
2019-01-07 11:21:15 +0800

    ...

--- resources/views/mails/welcome.blade.php
+++ resources/views/mails/welcome.blade.php
@@ -301,7 +301,7 @@
           <div class="content">
 
             <!-- START CENTERED WHITE CONTAINER -->
-            <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span>
+            <span class="preheader">Welcome to Enchanted Kingdom!</span>
 
             <table role="presentation" class="main">
               <!-- START MAIN CONTENT AREA -->
